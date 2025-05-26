977ea3ce6ac2caaa38a1d26cac64231a9764da37
Marwin Kreuzig <marwin@kreuzig.info>
2025-05-27 01:17:23 +0200

    experiment

--- flake.nix
+++ flake.nix
@@ -17,7 +17,28 @@
           programs.mcsr.enable = nixpkgs.lib.mkEnableOption "mcsr";
         };
         config = nixpkgs.lib.mkIf config.programs.mcsr.enable {
-          xdg.configFile."waywall/init.lua" = {};
+          xdg.configFile."waywall/init.lua".text= ''
+            local waywall = require("waywall")
+            local helpers = require("waywall.helpers")
+
+            local config = {
+                input = {
+                    layout = "us",
+                    repeat_rate = 40,
+                    repeat_delay = 300,
+
+                    sensitivity = 1.0,
+                    confine_pointer = false,
+                },
+                theme = {
+                    background = "#303030ff",
+                },
+            }
+
+            config.actions = {}
+
+            return config
+          '';
 
           home.packages = with pkgs; [
             obs-studio
