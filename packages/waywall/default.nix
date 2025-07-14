50107b9970b1d95f3f4a0e1d3551c9eb51655a99
Marwin Kreuzig <marwin@kreuzig.info>
2025-07-14 11:18:37 +0200

    waywall: update version

--- packages/waywall/default.nix
+++ packages/waywall/default.nix
@@ -1,7 +1,7 @@
 { pkgs, stdenv, lib }:
 stdenv.mkDerivation {
   pname = "waywall";
-  version = "0-unstable-2025-06-14";
+  version = "0-unstable-2025-07-14";
 
   patches = [
     ./ninjabrainbot-hack.patch
@@ -10,8 +10,8 @@ stdenv.mkDerivation {
   src = pkgs.fetchFromGitHub {
     owner = "tesselslate";
     repo = "waywall";
-    rev = "16607ea6ad34e62b19d3b8ce1d2fdda5a39d41ec";
-    hash = "sha256-1ZlyhacDm/8qyBPvpuqQKnSg+9qx78chUK+hH7+ahIY=";
+    rev = "90fafbe51e044738276688eb407913983044fccb";
+    hash = "sha256-ZaHZCIrhcbwbfV7xKmOjyuH06PwvCKAL+mkm+7gmNBI=";
   };
 
   nativeBuildInputs = with pkgs; [
