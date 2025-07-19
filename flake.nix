2bbe9497a61660034b1146370810f33ac33afc47
Marwin Kreuzig <marwin@kreuzig.info>
2025-07-19 11:48:06 +0200

    clean up flake structure

--- flake.nix
+++ flake.nix
@@ -6,45 +6,45 @@
     pinned-graal-nixpkgs.url = "github:nixos/nixpkgs/5ed627539ac84809c78b2dd6d26a5cebeb5ae269";
   };
 
-  outputs = { self, nixpkgs, pinned-graal-nixpkgs }:
+  outputs =
+    { self, nixpkgs, pinned-graal-nixpkgs }:
+    let
+      packages = pkgs: rec {
+        prismlauncher = (pkgs.prismlauncher.override { glfw3-minecraft = glfw-patched; });
+        modcheck = (pkgs.callPackage ./packages/modcheck/default.nix { });
+        ninjabrainbot = (pkgs.callPackage ./packages/ninjabrainbot/default.nix { });
+        waywall = pkgs.waywall.overrideAttrs (finalAttrs: previousAttrs: {
+          version = "0-unstable-2025-07-19";
+          src = pkgs.fetchFromGitHub {
+            owner = "tesselslate";
+            repo = "waywall";
+            rev = "992fbfd2fcc62570ee8e79a53efeb0e5b770ec4e";
+            hash = "sha256-b+Lc1VGpeNwcxRAaDvsE5Gkx7DVOl3HyHP19YJyH/OY=";
+          };
+        });
+        glfw-patched = pkgs.glfw.overrideAttrs (finalAttrs: previousAttrs: {
+          pname = "glfw-mcsr";
+          patches = previousAttrs.patches ++ [
+            (pkgs.fetchpatch
+              {
+                url = "https://raw.githubusercontent.com/tesselslate/waywall/be3e018bb5f7c25610da73cc320233a26dfce948/contrib/glfw.patch";
+                sha256 = "sha256-8Sho5Yoj/FpV7utWz3aCXNvJKwwJ3ZA3qf1m2WNxm5M=";
+              })
+          ];
+        });
+      };
+    in
     {
       packages."x86_64-linux" =
         let
           pkgs = import nixpkgs { system = "x86_64-linux"; };
         in
-        rec {
-          prismlauncher = (pkgs.prismlauncher.override { glfw3-minecraft = glfw-patched; });
-          modcheck = (pkgs.callPackage ./packages/modcheck/default.nix { });
-          ninjabrainbot = (pkgs.callPackage ./packages/ninjabrainbot/default.nix { });
-          waywall = (pkgs.callPackage ./packages/waywall/default.nix { });
-          glfw-patched = pkgs.glfw.overrideAttrs (finalAttrs: previousAttrs: {
-            pname = "glfw-mcsr";
-            patches = previousAttrs.patches ++ [
-              (pkgs.fetchpatch
-                {
-                  url = "https://raw.githubusercontent.com/tesselslate/waywall/be3e018bb5f7c25610da73cc320233a26dfce948/contrib/glfw.patch";
-                  sha256 = "sha256-8Sho5Yoj/FpV7utWz3aCXNvJKwwJ3ZA3qf1m2WNxm5M=";
-                })
-            ];
-          });
-        };
+        (packages pkgs);
       homeModules.mcsr =
         { config
         , pkgs
         , ...
         }:
-        let
-          glfw-patched = pkgs.glfw.overrideAttrs (finalAttrs: previousAttrs: {
-            pname = "glfw-mcsr";
-            patches = previousAttrs.patches ++ [
-              (pkgs.fetchpatch
-                {
-                  url = "https://raw.githubusercontent.com/tesselslate/waywall/be3e018bb5f7c25610da73cc320233a26dfce948/contrib/glfw.patch";
-                  sha256 = "sha256-8Sho5Yoj/FpV7utWz3aCXNvJKwwJ3ZA3qf1m2WNxm5M=";
-                })
-            ];
-          });
-        in
         {
           options.programs.mcsr = {
             enable = nixpkgs.lib.mkEnableOption "mcsr";
@@ -65,26 +65,10 @@
                   (obs-studio.override {
                     cudaSupport = true;
                   })
-                  (prismlauncher.override { glfw3-minecraft = glfw-patched; })
                   graalpkgs.graalvm-ce
-                  (callPackage ./packages/modcheck/default.nix { })
-                  (callPackage ./packages/ninjabrainbot/default.nix { })
-                  (callPackage ./packages/waywall/default.nix { })
-                  glfw-patched
-
-                  /*(waywall.overrideAttrs (finalAttrs: previousAttrs: {
-                buildInputs = previousAttrs.buildInputs ++ [
-                    # I thought these might somehow fix the ninjabrainbot issue but they don't ¯\_(ツ)_/¯
-                    xorg.libXcomposite
-                    xorg.libXres
-                ];
-                patches = (previousAttrs.patches or [ ]) ++ [
-                    ./ninjabrainbot-hack.patch
-                ];
-                  }))*/
                   # the presence of xwayland fixes performance issues when running waywall
                   xwayland
-                ];
+                ] ++ (pkgs.lib.attrsets.attrValues (packages pkgs));
 
                 xdg.configFile."waywall/init.lua".text = config.programs.mcsr.waywall.config;
               }
